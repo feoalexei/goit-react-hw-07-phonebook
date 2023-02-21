1f5665b086dd7c4fa375523d731f96a677d07c16
feoalexei <feoalexei@ukr.net>
2023-02-21 18:38:07 +0200

    added loader and final fixes

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -25,27 +25,26 @@ const contactSlice = createSlice({
     builder
       .addCase(getContactsThunk.pending, handlePending)
       .addCase(deleteContactThunk.pending, handlePending)
+      .addCase(addContactThunk.pending, handlePending)
+
       .addCase(getContactsThunk.rejected, handleRejected)
       .addCase(deleteContactThunk.rejected, handleRejected)
+      .addCase(addContactThunk.rejected, handleRejected)
 
       .addCase(getContactsThunk.fulfilled, (state, { payload }) => {
         state.items = payload;
         state.isLoading = false;
       })
-
       .addCase(deleteContactThunk.fulfilled, (state, { payload }) => {
         state.error = null;
         state.isLoading = false;
         state.items = state.items.filter(item => item.id !== payload);
       })
-
-      .addCase(addContactThunk.pending, handlePending)
       .addCase(addContactThunk.fulfilled, (state, { payload }) => {
         state.error = null;
         state.isLoading = false;
         state.items.push(payload);
-      })
-      .addCase(addContactThunk.rejected, handleRejected);
+      });
   },
 });
 
