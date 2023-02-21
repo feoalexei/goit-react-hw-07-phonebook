e4e8218f85b546f2f80b1f35ca349be06b057127
feoalexei <feoalexei@ukr.net>
2023-02-21 18:45:06 +0200

    deleted unused variables

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
         <App />
-        {/* </PersistGate> */}
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
